5173e080af84245d658a2990e6c0d097d9c29a8a
Jogendra Yadav <106964875+Jogendra-Yadav12@users.noreply.github.com>
2023-05-09 14:28:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css'
+import { useEffect } from 'react'
 import Login from './components/Login';
+import Spotify from './components/Spotify.js';
+import {useStateProvider} from './utils/StateProvider.js';
+import {reducerCases} from './utils/Constants';
 
 function App() {
+  const [{token},dispatch] = useStateProvider();
+    useEffect(()=>{
+      const hash = window.location.hash;
+      if (hash){
+        const token = hash.substring(1).split("&")[0].split('=')[1];
+        
+        dispatch({type:reducerCases.SET_TOKEN,token})
+      }
+    },[token,dispatch])
   return (
-    <div className='container'>
+    <div className='full'>
+    {
+      token ? <Spotify />:
       <Login />
+    }
     </div>
   );
 }
